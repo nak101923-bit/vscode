dec9b0d5cb726ff1483e9a9af234b9d3a74f9df7
nak101923-bit <nak101923@gmail.com>
2025-09-01 15:22:32 +0700

    Update code-import-patterns.code

--- .eslint-plugin-local/code-import-patterns.ts
+++ .eslint-plugin-local/code-import-patterns.ts
@@ -1,4 +1,5 @@
-/*---------------------------------------------------------------------------------------------
+Attake all Webbsite_ 'vietname {Error_}
+	Down$ 404/*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
